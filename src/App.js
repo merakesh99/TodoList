d9b7981d70ab9c4a9178b86f10366b0a6d21ad6a
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2021-07-14 22:14:35 +0530

    major_Update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Header from "./MyComponents/Header";
 import { Footer } from "./MyComponents/Footer";
 import { Todos } from "./MyComponents/Todos";
 import { AddTodo } from "./MyComponents/AddTodo";
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-=======
-import { About } from "./MyComponents/About";
 import React, { useState, useEffect } from 'react'; 
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,21 +11,13 @@ import {
 } from "react-router-dom";
 function App() {
   let initTodo;
-<<<<<<< HEAD
-  if (localStorage.getItem("todos") === null) {  
-=======
   if (localStorage.getItem("todos") === null) {
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
     initTodo = [];
   }
   else {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
-<<<<<<< HEAD
-  const onDelete = (todo) => { 
-=======
   const onDelete = (todo) => {
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
     setTodos(todos.filter((e) => {
       return e !== todo;
     }))
@@ -40,17 +27,10 @@ function App() {
   const addTodo = (title, desc) => {
     let sno;
     if (todos.length === 0) {
-<<<<<<< HEAD
-      sno = 1;  
-    }
-    else {
-      sno = todos[todos.length - 1].sno + 1; 
-=======
       sno = 1;
     }
     else {
       sno = todos[todos.length - 1].sno + 1;
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
     }
     const myTodo = {
       sno: sno,
@@ -58,10 +38,6 @@ function App() {
       desc: desc,
     }
     setTodos([...todos, myTodo]); 
-<<<<<<< HEAD
- 
-=======
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
   }
 
   const [todos, setTodos] = useState(initTodo);
@@ -72,26 +48,22 @@ function App() {
   return (
     <>
       <Router>
-        <Header title="TO-DO-list" />
+        <Header title="TODO list" Searchbar={false} />
 
         <Switch>
           <Route exact path="/TodoList" render={() => {
             return(
               <>
-            
-              <AddTodo addTodo={addTodo} />
+                <AddTodo addTodo={addTodo} />
                 <Todos todos={todos} onDelete={onDelete} />
-                  </>)
+              </>)
           }}>
             </Route>
+            
         </Switch>
         <Footer />
       </Router>
-         </>
+    </>
   );
 }
 export default App;
-<<<<<<< HEAD
-
-=======
->>>>>>> 0affdfa0fc501199bd544475afaeb4861d5dbe74
