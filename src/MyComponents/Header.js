a4c646d1b46d82df8d7bdcc36d002c557effcccc
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2022-06-23 19:48:14 +0530

    Navbar toggler remove

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -9,13 +9,13 @@ export default function Header(props) {
       <div className="container-fluid">
         <Link className="navbar-brand" to="/TodoList" >{props.title}</Link>
        
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           </ul>
-        </div>
+        </div> */}
         
         <DarkMode />
         
