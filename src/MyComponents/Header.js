edf941c5a143f30b90ca2a872af9344dfac301e6
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2022-06-23 19:07:33 +0530

    Navbar link added

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -7,7 +7,7 @@ export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/" >{props.title}</Link>
+        <Link className="navbar-brand" to="/TodoList" >{props.title}</Link>
        
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
